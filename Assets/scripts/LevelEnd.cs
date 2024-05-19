ba51aabfe5b94814e7590435dbe384ac6afe0f41
QuantumTrolling <st097173@student.spbu.ru>
2024-05-19 18:29:59 +0300

    FinalVer

--- Assets/scripts/LevelEnd.cs
+++ Assets/scripts/LevelEnd.cs
@@ -20,8 +20,8 @@ public class LevelEnd : MonoBehaviour
     }
 
    public IEnumerator Checker(){
-        yield return new WaitForSeconds(40);
-        if (resources.resource_stars >=100){
+        yield return new WaitForSeconds(45);
+        if (resources.resource_stars >=70){
             StartCoroutine(Nextlevel());
         }else{
             StartCoroutine(Replay());
@@ -34,7 +34,7 @@ public class LevelEnd : MonoBehaviour
         win.SetActive(true);
         yield return new WaitForSeconds(5);
         if (Level == 2){
-        SceneManager.LoadScene("Scene3");
+        SceneManager.LoadScene("Menu");
         }
         if (Level == 3){
             SceneManager.LoadScene("Menu");
